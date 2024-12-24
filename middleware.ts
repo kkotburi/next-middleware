be4553c6f6488faba7e63fdad2c4c5bf63813161
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-12-24 23:24:02 +0900

    Feat/middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// Middleware란? 요청과 응답 사이에 위치합니다.
 export function middleware(request: NextRequest) {
   const currentUrl = request.nextUrl.clone();
-  //   return NextResponse.redirect(new URL("/home", request.url));
+
+  // 1. Redirect to a new path
+  if (request.nextUrl.pathname === "/ping") {
+    currentUrl.pathname = "/pong";
+    return NextResponse.rewrite(currentUrl);
+  }
 }
 
 // export const config = {
